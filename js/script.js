c4fb07e168bfc0bc7c825cfcde008f349367ef92
ArcadiusBuzyk <buzyka99@gmail.com>
2023-06-04 19:41:37 +0200

    genres created by javaScript

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ let animeRanking = [
         title: "Gintama Season 4",
         rating: 97,
         img: "Gintama-Season-4.jpg",
-        views: 31385,
+        views: 66685,
         genres: [
             "action", "comedy", "drama", "sci-fi"
         ]
@@ -42,14 +42,14 @@ let animeRanking = [
         img: "Fruits-Basket-The-Final-Season.jpg",
         views: 31385,
         genres: [
-            "action", "comedy", "drama", "sci-fi"
+            "comedy", "drama", "psychological", "romance", 
         ]
     },
     {
         title: "Fullmetal Alchemist: Brotherhood",
         rating: 95,
         img: "Fullmetal-Alchemist-Brotherhood.jpg",
-        views: 31385,
+        views: 6060,
         genres: [
             "action", "adventure", "drama", "fantasy"
         ]
@@ -58,7 +58,7 @@ let animeRanking = [
         title: "Kaguya-sama: Love is War",
         rating: 94,
         img: "Kaguya-sama-Love-is-War.png",
-        views: 31385,
+        views: 22222,
         genres: [
             "comedy", "psychological", "romance"
         ]
@@ -67,37 +67,55 @@ let animeRanking = [
         title: "Gintama: THE VERY FINAL",
         rating: 98,
         img: "Gintama-THE-VERY-FINAL.jpg",
-        views: 31385
+        views: 10000,
+        genres: [
+            "action", "comedy", "drama", "sci-fi"
+        ]
     },
     {
         title: "Attack on Titan Season 3 Part 2",
         rating: 93,
         img: "Attack-on-Titan-Season-3-Part-2.jpg",
-        views: 31385
+        views: 4206,
+        genres: [
+            "action", "drama", "fantasy", "mystery"
+        ]
     },
     {
         title: "March comes in like a lion Season 2",
         rating: 92,
         img: "March-comes-in-like-a-lion-Season-2.jpg",
-        views: 31385
+        views: 2137,
+        genres: [
+            "drama"
+        ]
     },
     {
         title: "Hunter x Hunter (2011)",
         rating: 91,
         img: "Hunter-x-Hunter-(2011).png",
-        views: 103039
+        views: 103039,
+        genres: [
+            "action", "adventure", "fantasy"
+        ]
     },
     {
         title: "BLEACH: Thousand-Year Blood War",
         rating: 90,
         img: "BLEACH-Thousand-Year-Blood-War.jpg",
-        views: 31385
+        views: 31385,
+        genres: [
+            "action", "adventure", "supernatural"
+        ]
     },
     {
         title: "Owarimonogatari Second Season",
         rating: 89,
         img: "Owarimonogatari-Second-Season.png",
-        views: 31385
+        views: 9999,
+        genres: [
+            "comedy", "mystery", "psychological", "romance", "supernatural"
+        ]
     }
     
 ]
@@ -108,59 +126,73 @@ animeRanking.sort(function(a, b) {
     return b.rating - a.rating; // Sort in descending order
 });
 
-/* Top 10 anime title layout*/
+/* Top 10 anime */
 for (let i = 0; i < animeRanking.length; i++) {
     let anime = animeRanking[i];
 
+
+    /* title layout */
     const layout = document.querySelectorAll(".ellipsis");
 
     layout.forEach((l, index) => {
         if (index === i) {
             l.innerHTML = anime.title;  
         }
-    })
-}
+    });
 
-/* Top 10 anime img layout*/
-for (let i = 0; i < animeRanking.length; i++) {
-    let anime = animeRanking[i];
-
+    /* img layout */
     const ImageLayout = document.querySelectorAll(".image");
 
     ImageLayout.forEach((l, index) => {
         if (index === i) {
             l.src = "main-page-photos/top-100/" + anime.img;  
         }
-    })
-}
+    });
 
-/* Top 10 anime ratings layout */
-for (let i = 0; i < animeRanking.length; i++) {
-    let anime = animeRanking[i];
 
+    /* ratings layout */
     const RatingLayout = document.querySelectorAll(".percentage-score");
 
     RatingLayout.forEach((l, index) => {
         if (index === i) {
             l.innerHTML = anime.rating + "%";  
         }
-    })
-}
+    });
 
-
-/* Top 10 anime views layout*/
-for (let i = 0; i < animeRanking.length; i++) {
-    let anime = animeRanking[i];
-
+    /* views layout */
     const ViewsLayout = document.querySelectorAll(".popularity");
 
     ViewsLayout.forEach((l, index) => {
         if (index === i) {
-            l.innerHTML = anime.views;  
+            l.innerHTML = anime.views + " users";  
         }
     })
+
 }
 
+
+/* Genres  */
+
+for (let i = 0; i < animeRanking.length; i++) {
+    let anime = animeRanking[i];
+    let genres = anime.genres
+    const genresLayout = document.querySelectorAll(".genres")
+
+
+    genresLayout.forEach((l, index) => {
+        if (index === i) { 
+
+            genres.forEach((x) => {
+
+                let anchor = document.createElement("a")
+                anchor.classList.add("genre")
+                l.appendChild(anchor)
+                anchor.textContent = x
+            })
+        }
+    })
+
+}
 
 
 
